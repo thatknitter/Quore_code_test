60ef9e931c6041822167f7305023dafd7ff60b53
Kimberly Lewis <kblewis.flt@gmail.com>
2016-02-01 13:07:35 -0600

    framework built

--- app/main.js
+++ app/main.js
@@ -1,1 +1,5 @@
-ng.platform.browser.bootstrap(app.AppComponent);
+(function(app) {
+    document.addEventListener('DOMContentLoaded', function() {
+        ng.platform.browser.bootstrap(app.AppComponent);
+    });
+})(window.app || (window.app = {}));
